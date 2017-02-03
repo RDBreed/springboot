8f35d1d13441dda0b77be2f3521f71cff5656b5d
Tom Hofte <tomhofte@gmail.com>
2017-02-03 14:49:41 +0100

    Spring Boot Maven Archetype Added

---
+++ springbootarchetype/src/main/resources/archetype-resources/src/main/java/services/ServiceApplicationMain.java
@@ -0,0 +1,19 @@
+package services;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by tomhofte on 31/01/2017.
+ */
+@SpringBootApplication
+public class ServiceApplicationMain {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplicationMain.class, args);
+    }
+
+}
